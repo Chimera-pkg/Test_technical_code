86567679c5b8e705a5efb12f830432a3bae86836
unknown <adryanmagatsu88@gmail.com>
2024-02-02 10:08:51 +0700

    1.2

--- public/script.js
+++ public/script.js
@@ -1,11 +1,69 @@
 function generateTriangle () {
     const inputNumber = document.getElementById('inputNumber').value;
+
+    if(!/^\d+$/.test(inputNumber)) {
+        alert("input harus berupa angka positif");
+    };
+
+    // ajax
+    fetch('/generate-triangle', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({ inputNumber: inputNumber})
+    })
+    .then(response => response.json())
+    .then(data => {
+        // menampilkan result
+        document.getElementById('result').innerText = data.result;
+    })
+    .catch(error => console.error('Error bro', error));
 }
 
 function generateOddNumbers () {
     const inputNumber = document.getElementById('inputNumber').value;
+    if(!/^\d+$/.test(inputNumber)) {
+        alert("input harus berupa angka positif");
+    };
+
+    // ajax
+    fetch('/generate-ganjil', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({ inputNumber: inputNumber})
+    })
+    .then(response => response.json())
+    .then(data => {
+        // menampilkan result
+        document.getElementById('result').innerText = data.result;
+    })
+    .catch(error => console.error('Error bro', error));
 }
 
 function generatePrimeNumbers () {
     const inputNumber = document.getElementById('inputNumber').value;
+    if(!/^\d+$/.test(inputNumber)) {
+        alert("input harus berupa angka positif");
+    };
+
+    // ajax
+    fetch('/generate-prima', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({ inputNumber: inputNumber})
+    })
+    .then(response => response.json())
+    .then(data => {
+        // menampilkan result
+        document.getElementById('result').innerText = data.result;
+    })
+    .catch(error => console.error('Error bro', error));
 }
